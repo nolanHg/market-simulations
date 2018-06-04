7b9dbcd1502cd2d14f7593f0cb77519c71481a2b
Nolan Gagnon <nolan.gagnon@maine.edu>
2018-06-04 15:57:35 -0400

    now dynamically downloads most recent stock prices

--- market_simulation.R
+++ market_simulation.R
@@ -1,26 +1,59 @@
 library("ggplot2")
 library("gridExtra")
 library("rmutil")
+library("rvest")
 
 ################################
 #          CONSTANTS           #
 ################################
 TICK_SIZE <- 0.001
 
-## FUNCTION: distributes money supply in economy according to Pareto distribution
-distribute_wealth_pareto <- function(step_size = 0.1, supply = 1e9, n_traders = 500, loc = 1, disp = 3)
+## FUNCTION: gets actual price of stock from the Internet
+get_current_price <- function(ticker)
 {
+	url <- paste("https://finance.yahoo.com/quote/", ticker, "?p=", ticker, sep = "")
+	html_data <- read_html(url)
 
+	div_span_elems <- html_data %>% 
+		          html_nodes('div span') %>%
+		          html_text()	
+
+	current_price <- as.numeric(div_span_elems[11])
+	print(paste("--> ", ticker, " price download complete: ", "$", div_span_elems[11], " per share", sep = ""))
+
+	return(invisible(current_price))
+}
+
+
+## FUNCTION: distributes money supply in economy according to Pareto distribution
+distribute_wealth_pareto <- function(step_size = 0.1, supply = 1e12, n_traders = 500, loc = 1, disp = 1.5)
+{
+	## Allocate memory for vector
+	wealth_v <- rep(NA, n_traders)
+	
+	wealth_v[1] <- ppareto(step_size, loc, disp) * supply
+	wealth_v[2 : n_traders] <- supply * (ppareto(seq(from = 2 * step_size, by = step_size, length = n_traders - 1), loc, disp) -
+					     ppareto(seq(from = step_size, by = step_size, length = n_traders - 1), loc, disp))	
+
+	return(invisible(wealth_v))
 }
 
 ## FUNCTION: distributes shares of stock according to Pareto distribution
-distribute_shares_pareto <- function(step_size = 0.1, n_shares = 10000, n_traders = 500, loc = 1, disp = 3)
+distribute_shares_pareto <- function(step_size = 0.1, stock_names_v, n_shares_v, n_traders = 500, loc = 1, disp = 3)
 {
-	ownership_v <- rep(NA, n_traders)
-	ownership_v[1] <- ppareto(step_size, loc, disp) * n_shares 
-	ownership_v[2 : n_traders] <- n_shares * (ppareto(seq(from = 2 * step_size, by = step_size, length = n_traders - 1), loc, disp) -
+	## Allocate memory for data-frame
+	ownership_df <- data.frame(matrix(NA, nrow = n_traders, ncol = length(stock_names_v)))
+	colnames(ownership_df) <- stock_names_v
+
+	for (k in 1 : length(stock_names_v)) {	
+		ownership_v <- rep(NA, n_traders)
+		ownership_v[1] <- ppareto(step_size, loc, disp) * n_shares_v[k]
+		ownership_v[2 : n_traders] <- n_shares_v[k] * (ppareto(seq(from = 2 * step_size, by = step_size, length = n_traders - 1), loc, disp) -
 				                  ppareto(seq(from = step_size, by = step_size, length = n_traders - 1), loc, disp))
-	return(ownership_v)
+		ownership_df[stock_names_v[k]] <- ownership_v
+	}
+
+	return(invisible(ownership_df))
 }
 
 ## FUNCTION: Displays historical stock charts after completion of simulation
@@ -61,13 +94,16 @@ market_sim <- function(n_steps = 500, n_hft_traders = 1000, n_fund_traders = 100
 	goos_hist <- rep(NA, n_steps)
 	ups_hist <- rep(NA, n_steps)
 	jpm_hist <- rep(NA, n_steps)
+	
+	print("Downloading current stock prices ...")
+	gs_hist[1] <- get_current_price("GS")
+	bac_hist[1] <- get_current_price("BAC") 
+	swks_hist[1] <- get_current_price("SWKS") 
+	goos_hist[1] <- get_current_price("GOOS") 
+	ups_hist[1] <- get_current_price("UPS") 
+	jpm_hist[1] <- get_current_price("JPM") 
+	print("All downloads complete. Running simulation ...")
 
-	gs_hist[1] <- 237.71
-	bac_hist[1] <- 30.54
-	swks_hist[1] <- 98.83
-	goos_hist[1] <- 39.24
-	ups_hist[1] <- 116.95
-	jpm_hist[1] <- 112.05
 	pdata_l <- list(gs_hist, bac_hist, swks_hist, goos_hist, ups_hist, jpm_hist)
 
 	gs_health <- list(p_vneg = 0.1, p_neg = 0.3, p_pos = 0.4, p_vpos = 0.2)
@@ -80,12 +116,12 @@ market_sim <- function(n_steps = 500, n_hft_traders = 1000, n_fund_traders = 100
 	comp_healths_l <- list(gs = gs_health, bac = bac_health, swks = swks_health, goos = goos_health, ups = ups_health, jpm = jpm_health)	
 	
 	## Number of shares outstanding for each company
-	gs_shrs_outs <- 10000 
-	bac_shrs_outs <- 10000 
-	swks_shrs_outs <- 10000 
-	goos_shrs_outs <- 10000 
-	ups_shrs_outs <- 10000 
-	jpm_shrs_outs <- 10000 
+	gs_shrs_outs <- 4000 
+	bac_shrs_outs <- 4000 
+	swks_shrs_outs <- 4000 
+	goos_shrs_outs <- 4000 
+	ups_shrs_outs <- 4000 
+	jpm_shrs_outs <- 4000 
 	
 	shrs_outs_v <- cbind(gs_shrs_outs, bac_shrs_outs, swks_shrs_outs, goos_shrs_outs, ups_shrs_outs, jpm_shrs_outs)
 
@@ -93,12 +129,12 @@ market_sim <- function(n_steps = 500, n_hft_traders = 1000, n_fund_traders = 100
 	## 		Set-up traders             ##
 	#############################################
 	n_traders <- n_hft_traders + n_fund_traders + n_mom_traders + n_rand_traders
-	tags_m <- cbind(rep("HFT", n_hft_traders), rep("FUND", n_fund_traders),
-		        rep("MOM", n_mom_traders), rep("RAND", n_rand_traders))
-	capital_v <- rep(1e5, n_traders)
-	
-	traders_df <- data.frame(1 : n_traders, capital_v, tags_m)  
-	
+	tags_m <- sample(c(rep("HFT", n_hft_traders), rep("FUND", n_fund_traders),
+		        rep("MOM", n_mom_traders), rep("RAND", n_rand_traders)))
+	capital_v <- distribute_wealth_pareto(n_traders = n_traders) 
+	ownership_df <- distribute_shares_pareto(n_traders = n_traders, stock_names_v = tickers_v, n_shares_v = shrs_outs_v)
+	traders_df <- data.frame(1 : n_traders, capital_v, tags_m, ownership_df)  
+	colnames(traders_df) <- c("ID", "CAPITAL", "STRATEGY", tickers_v)
 	
 	#############################################
 	##          Run market simulation	   ##
@@ -111,7 +147,6 @@ market_sim <- function(n_steps = 500, n_hft_traders = 1000, n_fund_traders = 100
 		if (time == Q1 || time == Q2 || time == Q3) {
 			q_results_l <- lapply(comp_healths_l, FUN = function(x) issue_earnings_report(p_vneg = x$p_vneg, p_neg = x$p_neg, 
 												      p_pos = x$p_pos, p_vpos = x$p_vpos))
-			print(q_results_l)
 		}
 
 		## Calculate supply and demand	
